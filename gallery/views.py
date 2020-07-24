a9a633ca0a239c8345d176ee0154b4b2d02bd7b9
Daniel Pina <daniel_pina18@hotmail.com>
2020-07-24 22:44:53 +0100

    Created index and details template with respective routs

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
+from .models import Image
 
 def index(request):
-    return HttpResponse("You're at the gallery index.")
+    image_list = Image.objects.all()
+    context = {'image_list': image_list}
+    return render(request, 'gallery/index.html', context)
+
+def details(request, image_id):
+    """ try:
+        image = Image.objects.get(pk=image_id)
+    except Image.DoesNotExist:
+        raise Http404("Image does not exist")
+    return render(request, 'gallery/details.html', {'image': image}) """
+    image = get_object_or_404(Image, pk=image_id)
+    return render(request, 'gallery/details.html', {'image': image})
+
+def upload(request):
+    return HttpResponse("You're at upload page.")
